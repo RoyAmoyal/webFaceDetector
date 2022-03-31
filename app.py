c90d960a6510b5d75bf38c15abc1608a263831ee
royamoyal <roy15957@gmail.com>
2022-04-01 02:03:51 +0300

    adding yolov3 support

--- app.py
+++ app.py
@@ -22,6 +22,54 @@ def index():
     return render_template('main.html')
 
 
+whT = 320
+confThreshold = 0.5
+nmsThreshold = 0.2
+
+#### LOAD MODEL
+## Coco Names
+classesFile = "coco.names"
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().split("\n")
+print(classNames)
+## Model Files
+modelConfiguration = "yolov3-tiny.cfg"
+modelWeights = "yolov3-tiny.weights"
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+
+    for i in indices:
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        # print(x,y,w,h)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+                    (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+    return img
+
+
 def readb64(base64_string):
     idx = base64_string.find('base64,')
     base64_string = base64_string[idx + 7:]
@@ -69,11 +117,16 @@ def image(data_image):
     # Detect faces
     # Draw rectangle around the faces
     start_time = time.time()
-    #frame = face_detector(frame)
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = face_recogintion(frame)
+    # frame = face_detector(frame)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # frame = face_recogintion(frame)
     print("--- %s seconds ---" % (time.time() - start_time))
-
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layersNames = net.getLayerNames()
+    outputNames = [(layersNames[i - 1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    frame = findObjects(outputs, frame)
     # frame = cv2.flip(frame,1)
     imgencode = cv2.imencode('.jpeg', frame, [cv2.IMWRITE_JPEG_QUALITY, 40])[1]
 
